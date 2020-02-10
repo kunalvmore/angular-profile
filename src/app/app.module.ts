cdce81f01bc669a27013b1ea00ad30b7b147561d
kunalvmore <mkunal065@gmail.com>
2020-02-10 23:44:26 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileImageComponent } from './profile-image/profile-image.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileImageComponent,
+    ProfileDetailsComponent,
+    IntroductionComponent,
+    SkillsComponent,
+    EducationComponent,
+    ProjectsComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
